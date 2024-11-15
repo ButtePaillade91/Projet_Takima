e378672f4f85ab7fbdb6ca4d2bc57dd55a21bda4
tfevrier <titouan.fevrier@epfedu.fr>
2024-11-15 12:40:25 +0100

    Pseudo

--- Front/src/app/home/home.component.ts
+++ Front/src/app/home/home.component.ts
@@ -6,8 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  // Propriété pour gérer l'état de la fenêtre modale
+  isModalOpen = false;
+
+  // Propriété pour capturer le pseudo du joueur
+  pseudo: string = '';
+
   // Méthode basique pour gérer les clics sur une cellule
   handleClick(i: number, j: number): void {
     console.log(`Cellule cliquée : Ligne ${i}, Colonne ${j}`);
+  }
+
+  // Méthode pour ouvrir la modale
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  // Méthode pour fermer la modale
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
+
+  // Méthode pour démarrer le jeu et envoyer le pseudo
+  startGame(): void {
+    if (this.pseudo) {
+      console.log('Pseudo du joueur:', this.pseudo);
+      // Vous pouvez envoyer ce pseudo au backend ici via un service HTTP
+      // Exemple : this.gameService.startGame(this.pseudo);
+
+      this.closeModal(); // Fermer la modale après avoir démarré le jeu
+    } else {
+      alert('Veuillez entrer un pseudo.');
+    }
   }
 }
